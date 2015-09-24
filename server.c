bb8c2749bbe6fc1f467a1a9ec2cc73821c18610b
Kyle <kyle.thompson228@gmail.com>
2015-09-24 01:47:49 -0400

    Renamed variable names for clarity.

--- server.c
+++ server.c
@@ -1,35 +1,35 @@
 #include "common.h"
 
 int main() {
-    int listenfd = 0, connfd = 0;
+    int server_socket = 0, client_socket = 0;
 
     struct sockaddr_in serv_addr;
 
     char sendBuff[BUFFER+1];
     char recvBuff[BUFFER+1]; 
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff));
 
     serv_addr.sin_family = AF_INET;    
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    serv_addr.sin_port = htons(PORT);    
+    serv_addr.sin_port = htons(PORT);  
+    
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);  
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    bind(server_socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    if (listen(listenfd, 10) == -1) {
+    if (listen(server_socket, 10) == -1) {
         printf("Failed to listen\n");
         return -1;
     }
 
-    connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
+    client_socket = accept(server_socket, (struct sockaddr*)NULL ,NULL);
 
     pid_t pid = fork();
     if (pid) {
         while (strncmp(recvBuff, "exit", 4) != 0) {
-            if(recv(connfd, recvBuff, sizeof(recvBuff), 0) < 0)
+            if(recv(client_socket, recvBuff, sizeof(recvBuff), 0) < 0)
                 printf("Error: Receive\nErrno: %d\n", errno);
             recvBuff[BUFFER] = '\n';
 
@@ -37,22 +37,22 @@ int main() {
         }
 
         wait(&pid);
-        close(connfd);
+        close(client_socket);
 
     } else {
         while (strncmp(sendBuff, "exit", 4) != 0) {
             fputs("(Server) Enter a message: ", stdout);
             fgets(sendBuff, sizeof(sendBuff), stdin);
 
-            if(send(connfd, sendBuff, sizeof(sendBuff), 0) < 0)
+            if(send(client_socket, sendBuff, sizeof(sendBuff), 0) < 0)
                 printf("Error: Send\nErrno: %d\n", errno);
         }
         
-        close(connfd);
+        close(client_socket);
         exit(0);
     }
 
-    close(listenfd); 
+    close(server_socket); 
 
     return 0;    
 
