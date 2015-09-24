0a5a92e30afc593529f625d649737e447e150584
Kyle <kyle.thompson228@gmail.com>
2015-09-24 01:40:27 -0400

    Reformatted code. (changed indentation to 4 spaces instead of 2, etc.)

--- client.c
+++ client.c
@@ -1,50 +1,48 @@
 #include "common.h"
- 
-int main(void) {
-  int sockfd = 0;
-  char recvBuff[BUFFER+1];
-  char sendBuff[BUFFER+1];
-  struct sockaddr_in serv_addr;
- 
-  memset(recvBuff, '0' ,sizeof(recvBuff));
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("\n Error : Could not create socket \n");
-    return 1;
-  }
- 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(PORT);
-  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    printf("\n Error : Connect Failed \n");
-    return 1;
-  }
- 
-  pid_t pid = fork();
-  if (pid) {
-    while (strncmp(recvBuff, "exit", 4) != 0) {
-      if(recv(sockfd, recvBuff, sizeof(recvBuff), 0) < 0)
-        printf("Error: Receive\nErrno: %d\n", errno);
-      recvBuff[BUFFER] = '\n';
 
-      printf("Server: %s", recvBuff);
+int main(void) {
+    int sockfd = 0;
+    char recvBuff[BUFFER+1];
+    char sendBuff[BUFFER+1];
+    struct sockaddr_in serv_addr;
+
+    memset(recvBuff, '0' ,sizeof(recvBuff));
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Error : Could not create socket \n");
+        return 1;
     }
-    
-    wait(&pid);
-    close(sockfd);
-    
-  } else {
-    while (strncmp(sendBuff, "exit", 4) != 0) {
-      fputs("(Client) Enter a message: ", stdout);
-      fgets(sendBuff, sizeof(sendBuff), stdin);
-      
-      if(send(sockfd, sendBuff, sizeof(sendBuff), 0) < 0)
-        printf("Error: Send\nErrno: %d\n", errno);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\n Error : Connect Failed \n");
+        return 1;
     }
-    close(sockfd);
-    exit(0);
-  }
-  
-  return 0;
+
+    pid_t pid = fork();
+    if (pid) {
+        while (strncmp(recvBuff, "exit", 4) != 0) {
+            if(recv(sockfd, recvBuff, sizeof(recvBuff), 0) < 0)
+                printf("Error: Receive\nErrno: %d\n", errno);
+            recvBuff[BUFFER] = '\n';
+
+        printf("Server: %s", recvBuff);
+        }
+
+        wait(&pid);
+        close(sockfd);
+
+    } else {
+        while (strncmp(sendBuff, "exit", 4) != 0) {
+            fputs("(Client) Enter a message: ", stdout);
+            fgets(sendBuff, sizeof(sendBuff), stdin);
+
+            if(send(sockfd, sendBuff, sizeof(sendBuff), 0) < 0)
+                printf("Error: Send\nErrno: %d\n", errno);
+        }
+        close(sockfd);
+        exit(0);
+    }
 }
--- server.c
+++ server.c
@@ -1,58 +1,59 @@
 #include "common.h"
 
 int main() {
-  int listenfd = 0, connfd = 0;
-  
-  struct sockaddr_in serv_addr;
- 
-  char sendBuff[BUFFER+1];
-  char recvBuff[BUFFER+1]; 
- 
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff));
+    int listenfd = 0, connfd = 0;
 
-  serv_addr.sin_family = AF_INET;    
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-  serv_addr.sin_port = htons(PORT);    
- 
-  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    struct sockaddr_in serv_addr;
 
-  if (listen(listenfd, 10) == -1) {
-    printf("Failed to listen\n");
-    return -1;
-  }
+    char sendBuff[BUFFER+1];
+    char recvBuff[BUFFER+1]; 
 
-  connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
-  
-  pid_t pid = fork();
-  if (pid) {
-    while (strncmp(recvBuff, "exit", 4) != 0) {
-      if(recv(connfd, recvBuff, sizeof(recvBuff), 0) < 0)
-        printf("Error: Receive\nErrno: %d\n", errno);
-      recvBuff[BUFFER] = '\n';
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-      printf("Client: %s", recvBuff);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff));
+
+    serv_addr.sin_family = AF_INET;    
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serv_addr.sin_port = htons(PORT);    
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    if (listen(listenfd, 10) == -1) {
+        printf("Failed to listen\n");
+        return -1;
     }
-    
-    wait(&pid);
-    close(connfd);
-    
-  } else {
-    while (strncmp(sendBuff, "exit", 4) != 0) {
-      fputs("(Server) Enter a message: ", stdout);
-      fgets(sendBuff, sizeof(sendBuff), stdin);
-      
-      if(send(connfd, sendBuff, sizeof(sendBuff), 0) < 0)
-        printf("Error: Send\nErrno: %d\n", errno);
-    }
-    close(connfd);
-    exit(0);
-  }
 
-  close(listenfd); 
- 
-  return 0;    
+    connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
+
+    pid_t pid = fork();
+    if (pid) {
+        while (strncmp(recvBuff, "exit", 4) != 0) {
+            if(recv(connfd, recvBuff, sizeof(recvBuff), 0) < 0)
+                printf("Error: Receive\nErrno: %d\n", errno);
+            recvBuff[BUFFER] = '\n';
+
+            printf("Client: %s", recvBuff);
+        }
+
+        wait(&pid);
+        close(connfd);
+
+    } else {
+        while (strncmp(sendBuff, "exit", 4) != 0) {
+            fputs("(Server) Enter a message: ", stdout);
+            fgets(sendBuff, sizeof(sendBuff), stdin);
+
+            if(send(connfd, sendBuff, sizeof(sendBuff), 0) < 0)
+                printf("Error: Send\nErrno: %d\n", errno);
+        }
+        
+        close(connfd);
+        exit(0);
+    }
+
+    close(listenfd); 
+
+    return 0;    
 
 }
