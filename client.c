8fcf52279733da6df88e14dc81a53e95cb4dab52
Kyle <kyle.thompson228@gmail.com>
2015-09-24 22:33:36 -0400

    Added use of wrapper functions.

--- client.c
+++ client.c
@@ -7,19 +7,14 @@ int main(void) {
     struct sockaddr_in serv_addr;
 
     memset(recvBuff, '0' ,sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    }
+
+    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Error : Connect Failed \n");
-        return 1;
-    }
+    
+    Connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
     pid_t pid = fork();
     if (pid) {
