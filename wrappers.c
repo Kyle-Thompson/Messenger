869f12f775f4df706e8368d522018a01fda68eea
Kyle <kyle.thompson228@gmail.com>
2015-09-24 22:12:04 -0400

    added use of errno

--- wrappers.c
+++ wrappers.c
@@ -3,7 +3,7 @@
 int Socket(int domain, int type, int protocol) {
     int socket_number = 0;
     if ((socket_number = socket(domain, type, protocol)) < 0) {
-        printf("Error: socket()\n");
+        printf("Error: socket()\nErrno: %d %s\n", errno, strerror(errno));
         exit(1);
     }
     return socket_number;
@@ -11,14 +11,14 @@ int Socket(int domain, int type, int protocol) {
 
 void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     if (bind(sockfd, addr, addrlen) < 0) {
-        printf("Error: bind()\n");
+        printf("Error: bind()\nErrno: %d %s\n", errno, strerror(errno));
         exit(1);
     }
 }
 
 void Listen(int sockfd, int backlog) {
     if (listen(sockfd, backlog) < 0) {
-        printf("Error: listen()\n");
+        printf("Error: listen()\nErrno: %d %s\n", errno, strerror(errno));
         exit(1);
     }
 }
@@ -26,7 +26,7 @@ void Listen(int sockfd, int backlog) {
 int Accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len) {
     int file_descriptor = 0;
     if ((file_descriptor = accept(socket, address, address_len)) < 0) {
-        printf("Error: accept()\n");
+        printf("Error: accept()\nErrno: %d %s\n", errno, strerror(errno));
         exit(1);
     }
     return file_descriptor;
