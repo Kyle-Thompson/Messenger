8bbfb761bd1bde09733c4efcb4243a57d8acb4fb
Kyle <kyle.thompson228@gmail.com>
2015-09-24 22:01:47 -0400

    added wrapper functions to clean up code.

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ C = gcc -Wall -std=c11 -o
 
 all:	client server
 
-client:	client.c errorChecking.c common.h
-	$(C) client client.c errorChecking.c
+client:	client.c common.h
+	$(C) client client.c wrappers.c
 
-server:	server.c errorChecking.c common.h
-	$(C) server server.c errorChecking.c
+server:	server.c common.h
+	$(C) server server.c wrappers.c
 
 clean:	
 	rm -f client server
---
+++ wrappers.c
@@ -0,0 +1,33 @@
+#include "common.h"
+
+int Socket(int domain, int type, int protocol) {
+    int socket_number = 0;
+    if ((socket_number = socket(domain, type, protocol)) < 0) {
+        printf("Error: socket()\n");
+        exit(1);
+    }
+    return socket_number;
+}
+
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if (bind(sockfd, addr, addrlen) < 0) {
+        printf("Error: bind()\n");
+        exit(1);
+    }
+}
+
+void Listen(int sockfd, int backlog) {
+    if (listen(sockfd, backlog) < 0) {
+        printf("Error: listen()\n");
+        exit(1);
+    }
+}
+
+int Accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len) {
+    int file_descriptor = 0;
+    if ((file_descriptor = accept(socket, address, address_len)) < 0) {
+        printf("Error: accept()\n");
+        exit(1);
+    }
+    return file_descriptor;
+}
