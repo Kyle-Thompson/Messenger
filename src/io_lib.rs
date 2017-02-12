8226645830699e806435195a5943ce0128c89e04
Kyle Thompson <kyle.thompson228@gmail.com>
2017-02-12 16:05:08 -0500

    Big update after not doing it for a long time

--- src/io_lib.rs
+++ src/io_lib.rs
@@ -1,12 +1,37 @@
-use std::io::{self, Write};
+//extern crate pancurses;
 
-pub fn read_line(mut string: &mut String) {
-    io::stdout().flush().expect("Could not flush buffer.");
-    io::stdin().read_line(&mut string).expect("Failed to read user input!");
+use std::io::{self, Write};
+use std::sync::mpsc::Sender;
+//use pancurses::*;
+
+pub struct InputHandlerSenders {
+    pub done: Sender<i32>,      // extra input done
+    pub output: Sender<String>, // input handler to output
+    pub sender: Sender<String>, // input handler to sender
 }
 
-pub fn read_prompted_line(mut string: &mut String, prompt: &str) {
-    print!("{}", prompt);
-    read_line(&mut string);
+pub struct IOHandler {
+    //window: pancurses::Window,
+}
+
+impl IOHandler {
+    pub fn new() -> IOHandler {
+        println!("initializing io handler");
+        io::stdout().flush().expect("Could not flush buffer.");
+
+        IOHandler{
+        //    window : pancurses::initscr(),
+        }
+    }
+
+    pub fn read_line(&self, mut string: &mut String) {
+        io::stdin().read_line(&mut string).expect("Failed to read user input!");
+    }
+
+    pub fn read_prompted_line(&self, mut string: &mut String, prompt: &str) {
+        print!("{}", prompt);
+        io::stdout().flush().expect("Could not flush buffer.");
+        self.read_line(&mut string);
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::net::{UdpSocket};
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_mut)]
+
+//extern crate rustc_serialize;
+
+use std::net::{UdpSocket, SocketAddr};
 use std::thread;
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{self, channel};
+use std::sync::mpsc::{channel};
 
 mod io_lib;
 // mod net_lib;
 
-
-//struct User {
-//    handle: String,
-//    password: String,
-    // public_key: String,
-    // private_key: String,
-//}
+static SERVER_ADDR: &'static str = "159.203.57.173:5000";
+static RECV_ADDR: &'static str = "127.0.0.1:5000";
+static SEND_ADDR: &'static str = "127.0.0.1:5001";
 
 fn main() {
+
+    let io_handle = io_lib::IOHandler::new();
    
     // to sender
     let (recv_handler_to_sender, sender_receive) = channel::<String>();
@@ -24,50 +28,69 @@ fn main() {
     let (recv_handler_to_output, output_receive) = channel::<String>();
     let input_handler_to_output = recv_handler_to_output.clone();
 
-    thread::Builder::new().name("receiver".to_string()).spawn(move || {
-        let mut socket = UdpSocket::bind("127.0.0.1:5000")
-            .expect("Couldn't bind to socket!");
-    
+    thread::Builder::new().name("output".to_string()).spawn(move || {
+
+    }).unwrap();
+
+    thread::Builder::new().name("input".to_string()).spawn(move || {
+        let (input_done, input_recv) = channel::<i32>();
+
         loop {
+            // move these three into a stuct for easy passing.
+            let senders = io_lib::InputHandlerSenders {
+                done   : input_done.clone(),
+                output : input_handler_to_output.clone(),
+                sender : input_handler_to_sender.clone()
+            };
             
+            let mut input = String::new();
+            io_handle.read_prompted_line(&mut input, "> ");
+
+            thread::spawn(move || {
+                handle_user_input(&input, &senders);
+            });
+
+            // blocks until the handler thread is done taking additional user input.
+            let _ = input_recv.recv().unwrap();
         }
-    });
+    }).unwrap();
+
+    thread::Builder::new().name("receiver".to_string()).spawn(move || {
+        let mut socket = UdpSocket::bind(RECV_ADDR)
+            .expect("Couldn't bind to socket!");
+   
+        loop {
+            let mut buffer = [0; 4096];
+            let (amt, src) = socket.recv_from(&mut buffer)
+                .expect("Didn't receive data.");
+
+            thread::spawn(move || {
+                receive_handler(&src, &mut buffer[..amt]);
+            });
+        }
+    }).unwrap();
 
     thread::Builder::new().name("sender".to_string()).spawn(move || {
-        let mut socket = Arc::new(Mutex::new(UdpSocket::bind("127.0.0.1:5001")
+        let mut socket = Arc::new(Mutex::new(UdpSocket::bind(SEND_ADDR)
             .expect("Couldn't bind to socket!")));
 
-        // how do get thread IDs to then send ACKs back to these threads.
+        // how do get thread IDs to then send ACKs back to these threads?
         loop {
             let data = sender_receive.recv();
             thread::spawn(move || {
                 
             });
         }
-    });
-    
-    thread::Builder::new().name("output".to_string()).spawn(move || {
+    }).unwrap();
 
-    });
-    
-    login();
 }
 
-fn login() {
-    
-    let mut username = String::new();
-    let mut password = String::new();
+fn receive_handler(src: &SocketAddr, buf: &mut [u8]) {
 
-    loop {
-        io_lib::read_prompted_line(&mut username, "username: ");
-        io_lib::read_prompted_line(&mut password, "password: ");
-
-        // TODO: add authentication check.
-        break;
-    }
-
-    //User { 
-    //    handle: username.trim().to_string(), 
-    //    password: password.trim().to_string()
-    //}
 }
+
+fn handle_user_input(data: &String, senders: &io_lib::InputHandlerSenders) {
+    println!("user entered: {}", data);
+    senders.done.send(0).unwrap();
+}
+
